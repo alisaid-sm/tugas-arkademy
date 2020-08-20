3674dd61f551b25cc202be4352158b6a2f160244
alisaid-sm <alisaid7170@gmail.com>
2020-08-20 13:18:02 +0700

    selesai

--- codeimplement1/tugas1.js
+++ codeimplement1/tugas1.js
@@ -2,23 +2,23 @@
 // 1. deteksi palindrom
 // 2. reverse word
 
-// const kata = 'Malam';
+// const kata = 7;
 
 // if (typeof (kata) != 'string') {
 //     console.log(`kata harus string`)
 // } else {
 //     const a = kata.toLowerCase()
 //     const b = kata.toLowerCase().split("").reverse().join("")
-//     if (a.length < 3) {
-//         console.log(`huruf tidak boleh kurang dari 3`)
+//     if (a.length < 2) {
+//         console.log(`huruf tidak boleh kurang dari 2`)
 //     } else if (a === b) {
 //         console.log(`kata ${kata} adalah Palindrom`)
 //     } else {
 //         console.log(`kata ${kata} bukan Palindrom`)
 //     }
 // }
-// "Saya belajar Javascript"
-const kalimat = "Saya belajar Javascript"
+
+const kalimat = 9
 
 if (typeof (kalimat) != 'string') {
     console.log(`kata harus string`)
--- codeimplement1/tugas2.js
+++ codeimplement1/tugas2.js
@@ -11,30 +11,45 @@
 // contoh output:
 // 55566914566956
 
+// Algoritma
+// 1. Mulai
+// 2. Input data integer
+// 3. Validasi apakah data yang dimasukkan berupa number
+//    jika iya lanjut, jika tidak selesai
+// 4. data integer diubah menjadi string dengan tostring
+//    dan di pisah berdasarkan angka nol dengan split
+// 5. setelah itu di map, yang didalam map data di split
+//    lagi perhuruf, lalu di sort, lalu dijoin
+// 6. lalu data tersebut di join lagi
+// 7. kamudian data diubah ke number dengan parseint
+// 8. output number
+// 9. selesai
+
 
 const divideAndSort = (integer) => {
 
-    const b = integer.toString().split("0")
-    if (b.length <= 3) {
-        const [satu, dua, tiga] = b
-        const sortsatu = satu.split("").sort()
-        const sortdua = dua.split("").sort()
-        const sorttiga = tiga.split("").sort()
-        const gabung = sortsatu.concat(sortdua, sorttiga).join("")
-        const number = Number(gabung)
-        // console.log(integer)
-        // console.log(b)
-        // console.log(satu)
-        // console.log(dua)
-        // console.log(tiga)
-        // console.log(sortsatu)
-        // console.log(sortdua)
-        // console.log(sorttiga)
-        // console.log(gabung)
+
+    if (typeof integer == 'number') {
+        const a = integer.toString().split("0")
+        const b = a.map(e => {
+            const c = e.split('').sort().join('')
+            return c
+        })
+
+        const d = b.join('')
+        const number = parseInt(d)
         console.log(number)
     } else {
-        console.log('nol harus kurang dari 2')
+        console.log('data harus number')
     }
 
 }
 divideAndSort(5956560159466056)
+
+// let integer = 5956560159466056
+
+// const coba = integer.toString().split("0")
+// const map = coba.map(e => e.split("").sort().join(''))
+// const ki = Number(map.join(''))
+
+// console.log(map)
